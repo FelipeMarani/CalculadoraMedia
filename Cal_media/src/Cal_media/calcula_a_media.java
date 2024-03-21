cbc3a6350a858b7c134db3e348807ce85e37b08b
FelipeMarani <felipejesusdasilva02@gmail.com>
2024-03-21 20:00:36 -0300

    update

---
+++ Cal_media/src/Cal_media/calcula_a_media.java
@@ -0,0 +1,22 @@
+package Cal_media;
+
+public class calcula_a_media {
+	
+	public static void main(String[] args) {
+		
+    double nt1 = 3.5;
+    double nt2 = 9.1;
+    double nt3 = 6.0;
+	
+    double soma = nt1 + nt2 + nt3;
+    
+    double media = soma / 3;
+    
+    System.out.println(soma);
+    
+    System.out.println(media);
+    
+		
+	}
+
+}
